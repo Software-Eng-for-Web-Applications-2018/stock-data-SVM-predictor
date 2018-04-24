f8db20e8e668ee6d95267e39801a357737c00f96
John Grun <john.grun@gmail.com>
2018-04-24 19:23:03 -0400

    Modified script to run all realtime models at once

--- RunModelServer.sh
+++ RunModelServer.sh
@@ -1,2 +1,11 @@
 #! /bin/sh 
-tensorflow_model_server --port=9001 --model_name=SVMMODEL --model_base_path=$(pwd)
+tensorflow_model_server --port=9020 --model_name=SVMMODEL --model_base_path=$(pwd)/SVM_AABA &
+tensorflow_model_server --port=9021 --model_name=SVMMODEL --model_base_path=$(pwd)/SVM_AAPL &
+tensorflow_model_server --port=9022 --model_name=SVMMODEL --model_base_path=$(pwd)/SVM_AMD &
+tensorflow_model_server --port=9023 --model_name=SVMMODEL --model_base_path=$(pwd)/SVM_AMZN &
+tensorflow_model_server --port=9024 --model_name=SVMMODEL --model_base_path=$(pwd)/SVM_C &
+tensorflow_model_server --port=9025 --model_name=SVMMODEL --model_base_path=$(pwd)/SVM_GOOG &
+tensorflow_model_server --port=9026 --model_name=SVMMODEL --model_base_path=$(pwd)/SVM_GOOGL &
+tensorflow_model_server --port=9027 --model_name=SVMMODEL --model_base_path=$(pwd)/SVM_INTC &
+tensorflow_model_server --port=9028 --model_name=SVMMODEL --model_base_path=$(pwd)/SVM_MSFT &
+tensorflow_model_server --port=9029 --model_name=SVMMODEL --model_base_path=$(pwd)/SVM_VZ &
